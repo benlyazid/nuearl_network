d5391ec7f882ce2a16cc9ff0eea459cfcec6cc6e
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-02-23 19:29:23 +0100

    working in train function

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy
-import random
-
+import scipy.special
 class nueralNetwork:
 
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learningrate):
@@ -8,11 +7,22 @@ class nueralNetwork:
         self.hnodes = hidden_nodes
         self.onodes = output_nodes
         self.lr = learningrate
-    
-    def train(self):
-        return
-    def query(self):
-        print(10)
+        self.w_i_h = numpy.random.rand(self.inodes, self.hnodes)
+        self.w_h_o = numpy.random.rand(self.hnodes, self.onodes)
+        self.activation_function = lambda x: scipy.special.expit(x)
 
-nueral = nueralNetwork(2, 3, 2, 0.001)
-nueral.query()
+    def train(self):
+        pass
+
+    def query(self, input_list):
+        inputs = numpy.array(input_list, ndmin=2).T
+        print(inputs)
+        hidden_inputs = numpy.dot(self.w_i_h, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.w_h_o, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        print(final_outputs)
+        return final_outputs
+
+nueral = nueralNetwork(3, 3, 3, 0.001)
+nueral.query([1.2, 0.5, -1.5])
